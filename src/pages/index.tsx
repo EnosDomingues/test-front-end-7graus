b3682e7e72ad1892791882b74d7355569fd12001
Enos Domingues <enosdomingues@gmail.com>
2021-05-24 18:40:36 -0300

    updating README.md

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -57,29 +57,31 @@ export default function Home() {
             <RandomPerson user={user} key={user.login?.uuid}/>
           ))}
         </Grid>
-        <Box>
-          {users.length > 0 && (
-            <Button
-              colorScheme="blackAlpha"
-              h="16"
-              maxW="200px"
-              bg="gray.600"
-              mb="10"
-              mt="10"
-              _hover={{
-                bg: 'gray.500'
-              }}
-              isLoading={isLoading}
-              loadingText="Loading"
-              onClick={() => handleLoadMoreUsers()}
-            >
-              Load more 10 users
-            </Button>
-          )}
-        </Box>
-        <Flex align="center" justify="center" w="100%">
-          <Text color="gray.400">Proudly made in 🇧🇷 by Enos Domingues</Text>
-        </Flex>
+        {users.length > 0 && (
+          <>
+            <Box>
+                <Button
+                  colorScheme="blackAlpha"
+                  h="16"
+                  maxW="200px"
+                  bg="gray.600"
+                  mb="10"
+                  mt="10"
+                  _hover={{
+                    bg: 'gray.500'
+                  }}
+                  isLoading={isLoading}
+                  loadingText="Loading"
+                  onClick={() => handleLoadMoreUsers()}
+                >
+                  Load more 10 users
+                </Button>
+            </Box>
+            <Flex align="center" justify="center" w="100%">
+              <Text color="gray.400">Proudly made in 🇧🇷 by Enos Domingues</Text>
+            </Flex>
+          </>
+        )}
       </Flex>
     </> 
   )
